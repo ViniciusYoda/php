7c403dad37f15e3ae2135a38fce4811933739de5
Vinícius Yoda <vyoda4604@gmail.com>
2024-08-18 11:16:57 -0300

    iniciando estudos no php

---
+++ w3school/tutorial/home.php
@@ -0,0 +1,9 @@
+<!-- PHP Tutorial
+Learn PHP
+
+PHP is a server scripting language, and a powerful tool for making dynamic and interactive Web pages.
+
+PHP is a widely-used, free, and efficient alternative to competitors such as Microsoft's ASP.
+Easy Learning with "PHP Tryit"
+
+With our online "PHP Tryit" editor, you can edit the PHP code, and click on a button to view the result. -->
